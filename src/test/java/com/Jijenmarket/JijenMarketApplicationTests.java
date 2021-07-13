5a44ea96d6a488e3403c8c846987ae3659524838
“MiguelFandinoC” <miguelfaca1222@gmail.com>
2021-07-13 18:03:41 -0500

    Añade los archivos y la estructura inicial del proyecto

---
+++ src/test/java/com/Jijenmarket/JijenMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Jijenmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JijenMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
